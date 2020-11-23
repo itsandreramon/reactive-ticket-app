1461a4dd6afada0ea22a7039cdc912e81c849330
André Thiele <thielea@th-brandenburg.de>
2020-11-23 17:24:01 +0100

    format code

--- app/src/main/java/com/saqs/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/saqs/app/ui/home/HomeFragment.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2020 - André Thiele
+ *
+ * Department of Computer Science and Media
+ * University of Applied Sciences Brandenburg
+ */
+
 package com.saqs.app.ui.home
 
 import android.os.Bundle
