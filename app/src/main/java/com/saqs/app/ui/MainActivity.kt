d0adb0bf8a0addf2667d271ccb06c05cb74070bd
André Thiele <thielea@th-brandenburg.de>
2020-11-30 21:24:43 +0100

    format

--- app/src/main/java/com/saqs/app/ui/MainActivity.kt
+++ app/src/main/java/com/saqs/app/ui/MainActivity.kt
@@ -9,7 +9,6 @@ package com.saqs.app.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
