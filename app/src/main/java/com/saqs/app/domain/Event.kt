30bef278ec13b6b061b7a556709247ee0510271c
André Thiele <thielea@th-brandenburg.de>
2020-12-03 20:42:49 +0100

    fix copyright, import

--- app/src/main/java/com/saqs/app/domain/Event.kt
+++ app/src/main/java/com/saqs/app/domain/Event.kt
@@ -1,16 +1,22 @@
+/*
+ * Copyright 2020 - André Thiele
+ *
+ * Department of Computer Science and Media
+ * University of Applied Sciences Brandenburg
+ */
+
 package com.saqs.app.domain
 
 import android.os.Parcelable
 import androidx.recyclerview.widget.DiffUtil
-import com.google.firebase.firestore.FirebaseFirestore
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
+import com.google.firebase.firestore.FirebaseFirestore
 import com.saqs.app.data.database.Converters
 import com.saqs.app.util.round
 import java.time.ZoneId
 import java.time.ZonedDateTime
-import kotlin.math.round
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -35,7 +41,7 @@ class Event(
             .div(availableTicketsOverall.toDouble())
             .round(2)
 
-    fun updateSelf(){
+    fun updateSelf() {
         val db = FirebaseFirestore.getInstance()
     }
 }
