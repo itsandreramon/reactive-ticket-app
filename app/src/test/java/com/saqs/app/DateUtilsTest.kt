b33b354c01d3a7a4cafd6b558a417f822be033dc
Bertin <b.wagueunkepgang@reply.de>
2020-12-06 15:26:56 +0100

    toUtcStringTest almost done

--- app/src/test/java/com/saqs/app/DateUtilsTest.kt
+++ app/src/test/java/com/saqs/app/DateUtilsTest.kt
@@ -1,25 +1,59 @@
 package com.saqs.app
 
-import com.saqs.app.util.DateUtils
 import com.saqs.app.util.DateUtils.toLocalFormattedDate
+import com.saqs.app.util.DateUtils.toUtcString
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import org.junit.Assert
-import org.junit.Test
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZonedDateTime
+import io.kotest.matchers.shouldNotBe
+import java.time.*
 import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.time.temporal.TemporalAccessor
+import java.util.*
 
 
-class DateUtilsTest : FunSpec( {
+class DateUtilsTest : FunSpec({
     test("toLocalFormattedDateTest") {
         val zoneId = ZoneId.of("Europe/Berlin")
         val expected = ZonedDateTime.of(2020, 12, 6, 13, 0, 0, 0, zoneId)
         val instant = expected.toInstant()
 
         val actual = toLocalFormattedDate(instant, zoneId)
-
+        System.out.println("Test instant " + Instant.now())
         actual shouldBe expected.format(DateTimeFormatter.ofPattern("EEE, d MMM yyyy"))
     }
+
+    test("toUtcStringTest") {
+
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+
+        val timestamp = "2020-12-06 15:20"
+        val temporalAccessor: TemporalAccessor = formatter.parse(timestamp)
+        val localDateTime = LocalDateTime.from(temporalAccessor)
+        val zonedDateTime = ZonedDateTime.of(localDateTime, ZoneOffset.UTC)
+        val expected = Instant.from(zonedDateTime)
+
+        val instant = Instant.now()
+        val actual = toUtcString(instant)
+
+
+        /**val zoneId = ZoneId.of("Europe/Berlin")
+
+        val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
+                .withLocale(Locale.GERMANY)
+                .withZone(zoneId)
+        val instant = Instant.now()
+        val expected: LocalDateTime = LocalDateTime.ofInstant(instant, ZoneOffset.UTC)
+
+
+        //val expected = ZonedDateTime.of(2020, 12, 6, 13, 0, 0, 0, zoneId)
+
+        val actual = toUtcString(instant)
+        //val instant: Instant = Instant.now();*/
+        System.out.println(expected)
+
+       actual shouldBe  expected
+
+    }
+
 })
