6d2f83f36bb26de03e3cb70d224fa14d1a8cb635
André Thiele <thielea@th-brandenburg.de>
2020-12-08 21:50:35 +0100

    add comments

--- app/src/test/java/com/saqs/app/util/DateUtilsTest.kt
+++ app/src/test/java/com/saqs/app/util/DateUtilsTest.kt
@@ -1,58 +1,86 @@
-package com.saqs.app
+/*
+ * Copyright 2020 - André Thiele, Allan Fodi, Hüseyin Celik, Bertin Junior Wagueu Nkepgang
+ *
+ * Department of Computer Science and Media
+ * University of Applied Sciences Brandenburg
+ */
 
+package com.saqs.app.util
+
+import com.google.firebase.Timestamp
 import com.saqs.app.util.DateUtils.fromTimestamp
 import com.saqs.app.util.DateUtils.fromUtcString
-import com.saqs.app.util.DateUtils.toLocalFormattedDate
 import com.saqs.app.util.DateUtils.toUtcString
-import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import java.time.*
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
-import java.util.*
+import java.util.Date
+import org.junit.Test
 
+class DateUtilsTest {
 
-class DateUtilsTest : FunSpec({
-    test("toLocalFormattedDateTest") {
+    @Test
+    fun toLocalFormattedDateTest() {
+        // Given
         val zoneId = ZoneId.of("Europe/Berlin")
         val expected = ZonedDateTime.of(2020, 12, 6, 13, 0, 0, 0, zoneId)
         val instant = expected.toInstant()
 
-        val actual = toLocalFormattedDate(instant, zoneId)
-        println("Test instant " + Instant.now())
+        // When
+        val actual = DateUtils.toLocalFormattedDate(instant, zoneId)
+
+        // Then
         actual shouldBe expected.format(DateTimeFormatter.ofPattern("EEE, d MMM yyyy"))
     }
 
-    test("toUtcStringTest") {
+    @Test
+    fun toUtcStringTest() {
+        // Given
         val zoneId = ZoneId.of("Europe/Berlin")
-        val date = ZonedDateTime.of(2020,12,6,13,0,0,0,zoneId)
+        val date = ZonedDateTime.of(2020, 12, 6, 13, 0, 0, 0, zoneId)
 
         val expected = DateTimeFormatter
                 .ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
                 .withZone(ZoneOffset.UTC)
                 .format(date)
 
+        // When
         val actual = toUtcString(date.toInstant())
+
+        // Then
         actual shouldBe expected
     }
 
-    test("fromUtcStringTest"){
+    @Test
+    fun fromUtcStringTest() {
+        // Given
         val zoneId = ZoneId.of("Europe/Berlin")
-        val expected = ZonedDateTime.of(2020,12,6,13,0,0,0,zoneId)
+        val expected = ZonedDateTime.of(2020, 12, 6, 13, 0, 0, 0, zoneId)
 
         val expectedAsString = DateTimeFormatter
                 .ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
                 .withZone(ZoneOffset.UTC)
                 .format(expected)
 
+        // When
         val actual = fromUtcString(expectedAsString)
+
+        // Then
         actual shouldBe expected.toInstant()
     }
 
-    test("fromTimestampTest"){
-        val timestamp = com.google.firebase.Timestamp(Date(2020,12,6,14,13,50))
-        val actual = fromTimestamp(timestamp)
+    @Test
+    fun fromTimestampTest() {
+        // Given
+        val timestamp = Timestamp(Date(2020, 12, 6, 14, 13, 50))
         val expected = timestamp.toDate().toInstant()
+
+        // When
+        val actual = fromTimestamp(timestamp)
+
+        // Then
         actual shouldBe expected
     }
-
-})
+}
